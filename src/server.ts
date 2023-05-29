8d4e47707274ccb01028e063d54e09e186aeb7cd
Lídia Galdino <98404088+lidiagaldino@users.noreply.github.com>
2023-05-29 09:26:25 -0300

    finalizado

--- src/server.ts
+++ src/server.ts
@@ -9,46 +9,44 @@ const port = normalizePort(process.env.PORT || "3000");
 console.log(port);
 
 app.io.on("connection", (socket) => {
-    console.log(`CLIENTE CONECTADO ${socket.id}`);
+  console.log(`CLIENTE CONECTADO ${socket.id}`);
 
-    let decoded: IPayload = { id_usuario: 0, id_modo: 0, user_type: 'GERADOR' };
+  let decoded: IPayload = { id_usuario: 0, id_modo: 0, user_type: "GERADOR" };
 
-    if (!socket.handshake.auth || !socket.handshake.auth.token) socket.disconnect()
+  if (!socket.handshake.auth || !socket.handshake.auth.token)
+    socket.disconnect();
 
-    try {
-        decoded = jwt.verify(socket.handshake.auth.token, "secret") as IPayload;
-        console.log(decoded);
-    } catch (error) {
-        socket.emit("InvalidToken", "token invalido");
-        socket.disconnect();
-    }
+  try {
+    decoded = jwt.verify(socket.handshake.auth.token, "secret") as IPayload;
+    console.log(decoded);
+  } catch (error) {
+    socket.emit("InvalidToken", "token invalido");
+    socket.disconnect();
+  }
 
-    try {
-        socket.join(`user_${decoded.id_usuario}`)
-    } catch (error) {
-        socket.disconnect();
-    }
+  try {
+    socket.join(`user_${decoded.id_usuario}`);
+  } catch (error) {
+    socket.disconnect();
+  }
 
-    socket.on("send-msg", (data) => {
-        socket.to(data.to).emit("msg-recieve", data.msg)
-    })
-
-
-
+  socket.on("send-msg", (data) => {
+    socket.to(`user_${data.to}`).emit("msg-recieve", data.msg);
+  });
 });
 
 app.httpServer.listen(port, () => console.log("App rodando"));
 
 function normalizePort(val: string) {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 }
